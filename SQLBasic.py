85ceb3b9316674bfa4c466a669d5639f37df2523
Andrej <andrej.savinov@campus.tu-berlin.de>
2018-05-29 17:28:31 +0200

    update#2

--- SQLBasic.py
+++ SQLBasic.py
@@ -1,16 +1,16 @@
 import sqlalchemy as sqla
 import pymysql
+import MySql as Database
 import numpy as np
 import csv
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import *
 
 def main():
-    print("Test")
     try:
         engine = create_engine('mysql+pymysql://dwdtestuser:asdassaj14123@weather.service.tu-berlin.de/dwdtest?use_unicode=1&charset=utf8&ssl_cipher=AES128-SHA')
         print("Engine creation successful")
-        connection = engine.connect
+        connection = engine.connect()
         print("Connection successful")
     except Exception as error:
         if "Engine not found" in str(error):
@@ -22,7 +22,11 @@ def main():
     connection.close()
 
 def SQLpostcode(postcode,connection):
-    select = sqla.select([pymysql.Dwd]).where(pymysql.dwd.postcode == postcode)
+    select = sqla.select([Database.Dwd]).where(Database.Dwd.postcode == postcode)
     result = connection.execute(select)
+    print(type(result))
     for row in result:
         print(row)
+
+if __name__ == '__main__':
+    main()
