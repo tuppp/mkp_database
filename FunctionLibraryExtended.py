302c817bf55310d2614906f890af5a7461352913
Do (Ha) Phuong <dohaphuong@do.haphuong>
2018-06-05 17:20:02 +0200

    Revert "Merge branch 'master' of https://github.com/tuppp/mkp_database"
    This reverts commit 01140464b2485ff29918e277c818791071e92f84, reversing
    changes made to 2e040ac2ed0de1835b5b8622e35fb1c7100eac48.
    
    Reverting a mess

--- FunctionLibraryExtended.py
+++ FunctionLibraryExtended.py
@@ -3,20 +3,6 @@ import sqlalchemy as sqla
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 
-<<<<<<< HEAD
-# auxiliary functions
-
-# convert a list of column name strings into parameters to call, if no columns given: return the whole table
-def getColumnList(columnlist, table, se):
-    if not columnlist:
-        return table
-    parameters = []
-    for col in columnlist:
-        parameters.append(getattr(table.c, col))
-    return parameters
-
-=======
->>>>>>> 2cc02420f8e9ec5b16dab0f1034ce92db9f05124
 
 # Functions
 
@@ -39,29 +25,6 @@ def getRain(table, se, query=None):
         return query
 
 # sunny days (x sun hours)
-<<<<<<< HEAD
-def getSunHours(postcode, table, sunHours, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.sun_hours >= sunHours):
-        result.append(a)
-    return np.vstack(result)
-
-
-# sunny days (x sun hours) by date
-def getSunHoursByDate(date, table, sunHours, se):
-    result = []
-    for a in se.query(table).filter(table.c.measure_date == date).filter(table.c.sun_hours >= sunHours):
-        result.append(a)
-    return np.vstack(result)
-
-
-# hot days (x tempavg)
-def getTempAvg(postcode, table, avgTemp, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.average_temp >= avgTemp):
-        result.append(a)
-    return np.vstack(result)
-=======
 def getSunHours(sunHours, table, se, query=None):
     if(query == None):
         query = se.query(table).filter(table.c.sun_hours == sunHours)
@@ -88,28 +51,25 @@ def getTempAvg(avgTemp, table, se, query=None):
         query = query.filter(table.c.average_temp == avgTemp)
         return query
 
->>>>>>> 2cc02420f8e9ec5b16dab0f1034ce92db9f05124
-
 
 # hot days (x tempavg)
-def getTempAvgByDate(date, table, avgTemp, se):
+def getTempAvgByDate(date, avgTemp, table, se):
     result = []
     for a in se.query(table).filter(table.c.measure_date == date).filter(table.c.average_temp >= avgTemp):
         result.append(a)
     return np.vstack(result)
 
-##########################################
 
 # a lot of rain
-def getMorePercipitation(columlist, postcode, number, table, se):
+def getPrecAvg(postcode, avgPrec, table, se):
     result = []
-    for a in se.query(*getColumnList(columlist, table, se)).filter(table.c.postcode == postcode).filter(table.c.precipitation_amount >= avgPrec):
+    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.precipitation_amount >= avgPrec):
         result.append(a)
     return np.vstack(result)
 
 
 # look for precipitation type
-def getPrecType(postcode, table, precipitationType):
+def getPrecType(postcode, precipitationType, table):
     result = []
     for a in se.query(table).filter(table.c.postcode == postcode).filter(
             table.c.precipitation_type == precipitationType):
@@ -118,65 +78,6 @@ def getPrecType(postcode, table, precipitationType):
 
 
 # look for date
-<<<<<<< HEAD
-def getDate(table, date, se):
-    result = []
-    for a in se.query(table).filter(table.c.measure_date == date):
-        result.append(a)
-    return np.vstack(result)
-
-
-# look for date and postcode
-def getDateAndPostcode(postcode, table, date, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.measure_date == date):
-        result.append(a)
-    return np.vstack(result)
-
-
-# look for max wind
-def getMaxWind(postcode, table, maxWind, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.max_wind_speed >= maxWind):
-        result.append(a)
-    return np.vstack(result)
-##########################################
-
-# look for snow
-def getSnowHeight(postcode, table, snow, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.snow_height >= snow):
-        result.append(a)
-    return np.vstack(result)
-
-
-# look for avg wind speed
-def getWindSpeedAvg(postcode, table, avgWindSpeed, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.average_wind_speed >= avgWindSpeed):
-        result.append(a)
-    return np.vstack(result)
-
-
-# look for max temp
-def getMaxTemp(postcode, table, maxTemp, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.max_temp >= maxTemp):
-        result.append(a)
-    return np.vstack(result)
-
-
-# look for min # temp
-def getMinTemp(postcode, table, minTemp, se):
-    result = []
-    for a in se.query(table).filter(table.c.postcode == postcode).filter(table.c.min_temp >= minTemp):
-        result.append(a)
-    return np.vstack(result)
-
-
-# look for coverage
-def getCoverage(postcode, table, coverage, se):
-=======
 def getDate(date, table, se, query=None):
     if query is None:
         query = se.query(table).filter(table.c.measure_date == date)
@@ -292,7 +193,6 @@ def getCoverageDown(coverage, table, se, query=None):
 
 
 def getResult(query):
->>>>>>> 2cc02420f8e9ec5b16dab0f1034ce92db9f05124
     result = []
     for a in query:
         result.append(a)
@@ -303,53 +203,6 @@ def getResult(query):
 
 ####################################################
 
-<<<<<<< HEAD
-Base = declarative_base()
-engine = create_engine(
-    'mysql+pymysql://dwdtestuser:asdassaj14123@weather.service.tu-berlin.de/dwdtest?use_unicode=1&charset=utf8&ssl_cipher=AES128-SHA')
-# mysql --ssl-cipher=AES128-SHA -u dwdtestuser -p -h weather.service.tu-berlin.de dwdtest
-# mysql --ssl -u dwdtestuser -p -h weather.service.tu-berlin.de dwdtest
-# password asdassaj14123
-# use dwdtest
-Base.metadata.create_all(engine)
-Session = sqla.orm.sessionmaker()
-Session.configure(bind=engine)
-se = Session()
-# connect to database
-metadata = MetaData(engine, reflect=True)
-
-# Get Table
-Dwd = metadata.tables['dwd']
-
-#Test functions
-
-print("Tage mit mindestens x Niederschlagsmenge")
-print(getMorePercipitation(["station_id", "station_name", "postcode", "precipitation_amount"], 26197, 1.0, Dwd, se))
-print()
-
-print("Nach Datum suchen")
-print(getDate(Dwd, 20171013, se))
-print()
-
-# print("Nach snowHeight suchen")
-# print(getSnowHeight(26197, Dwd, 0.1, se))
-# print()
-
-# print("Nach maxTemp suchen")
-# print(getMaxTemp(26197, Dwd, 25.0, se))
-# print()
-
-# print("Nach minTemp suchen")
-# print(getMinTemp(26197, Dwd, 5.0, se))
-# print()
-
-# print("Nach Datum und Postcode suchen")
-# print(getDateAndPostcode(26197, Dwd, 20170512, se))
-# print()
-
-
-se.close()
-=======
 def main():
     try:
         Base = declarative_base()
@@ -412,4 +265,3 @@ def main():
 
 if __name__ == '__main__':
     main()
->>>>>>> 2cc02420f8e9ec5b16dab0f1034ce92db9f05124
