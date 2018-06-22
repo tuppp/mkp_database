8a110dcc375e201bedcd4c86f113e2d0f11e6ec5
Ernest Astfalk <ernest.astfalk@1und1.de>
2018-06-22 11:56:14 +0200

    fixed some functions / added path/function for login

--- FunctionLibraryExtended.py
+++ FunctionLibraryExtended.py
@@ -1,8 +1,12 @@
 import numpy as np
 import sqlalchemy as sqla
+
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 
+
+#Path to userdata file
+path_to_login_file = "Login/example_login_data"
 
 # Functions
 def getPostcode(postcode, table, se, query=None):
@@ -22,7 +26,6 @@ def getPostcode(postcode, table, se, query=None):
         return query
 
 
-# rainy days
 def getRain(table, se, query=None):
     r"""
         Returns rain information
@@ -69,7 +72,6 @@ def getMinSunHours(table, se, query=None):
         return query.query(func.min(table.c.sun_hours))
 
 
-# sunny days (x sun hours)
 def getSunHours(sunHours, table, se, query=None):
     r"""
        Method gets all entries with matching amount of sun
@@ -86,7 +88,7 @@ def getSunHours(sunHours, table, se, query=None):
         query = query.filter(table.c.sun_hours == sunHours)
         return query
 
-# hot days (x tempavg)
+
 def getTempAvg(avgTemp, table, se, query=None):
     r"""
        Method gets all entries with desired average temperature
@@ -96,12 +98,22 @@ def getTempAvg(avgTemp, table, se, query=None):
        :param query: Query Object which contains SQL query, if empty one will be created
        :returns: Query Object, can be reused for other queries
        """
-    if (query == None):
-        query = se.query(table).filter(table.c.average_temp == avgTemp)
-        return query
+
+    if(str(table) == "dwd"):
+        if (query == None):
+            query = se.query(table).filter(table.c.average_temp == avgTemp)
+            return query
+        else:
+            query = query.filter(table.c.average_temp == avgTemp)
+            return query
     else:
-        query = query.filter(table.c.average_temp == avgTemp)
-        return query
+        if (query == None):
+            query = se.query(table).filter(table.c.temp == avgTemp)
+            return query
+        else:
+            query = query.filter(table.c.temp == avgTemp)
+            return query
+
 
 def getMaxPrec(table, se, query=None):
     r"""
@@ -133,7 +145,6 @@ def getMinPrec(table, se, query=None):
         return query.query(func.min(table.c.precipitation_amount))
 
 
-# a lot of rain
 def getPrecAvg(avgPrec, table, se, query=None):
     r"""
        Method gets all entries with average precipitation higher than/equal to input average precipitation
@@ -160,7 +171,7 @@ def getMaxWindUp(maxWind, table, se, query=None):
     :param query: Query Object which contains SQL query, if empty one will be created
     :returns: Query Object, can be reused for other queries
     """
-    if(table == "dwd"):
+    if(str(table) == "dwd"):
         if query is None:
             query = se.query(table).filter(table.c.max_wind_speed >= maxWind)
             return query
@@ -171,7 +182,7 @@ def getMaxWindUp(maxWind, table, se, query=None):
         print("No maxwind available!")
         return query
 
-# look for precipitation type
+
 def getPrecType(precipitationType, table, se, query=None):
     r"""
        Method gets all entries with given pecipitation type
@@ -187,6 +198,7 @@ def getPrecType(precipitationType, table, se, query=None):
     else:
         query = query.filter(table.c.precipitation_type == precipitationType)
         return query
+
 
 def getDate(date, table, se, query=None):
     r"""
@@ -204,6 +216,7 @@ def getDate(date, table, se, query=None):
         query = query.filter(table.c.measure_date == date)
         return query
 
+
 def getAvgWindSpeedUp(avgWind, table, se, query=None):
     r"""
     get all data from table with average_wind_speed >= given value
@@ -213,7 +226,7 @@ def getAvgWindSpeedUp(avgWind, table, se, query=None):
     :param query: Query Object which contains SQL query, if empty one will be created
     :returns: Query Object, can be reused for other queries
     """
-    if(table == "dwd"):
+    if(str(table) == "dwd"):
         if query is None:
             query = se.query(table).filter(table.c.average_wind_speed >= avgWind)
             return query
@@ -228,6 +241,7 @@ def getAvgWindSpeedUp(avgWind, table, se, query=None):
             query = query.filter(table.c.max_wind_speed >= avgWind)
             return query
 
+
 def getAvgWindSpeedDown(avgWindSpeed, table, se, query):
     r"""
     get all data from table with average_wind_speed <= given value
@@ -237,7 +251,7 @@ def getAvgWindSpeedDown(avgWindSpeed, table, se, query):
     :param query: Query Object which contains SQL query, if empty one will be created
     :returns: Query Object, can be reused for other queries
     """
-    if(table == "dwd"):
+    if(str(table) == "dwd"):
         if query is None:
             query = se.query(table).filter(table.c.average_wind_speed <= avgWindSpeed)
             return query
@@ -252,7 +266,7 @@ def getAvgWindSpeedDown(avgWindSpeed, table, se, query):
             query = query.filter(table.c.max_wind_speed <= avgWindSpeed)
             return query
 
-# look for snow height >=
+
 def getSnowHeightUp(snow, table, se, query=None):
     r"""
     get all data from table with snow_height >= given value
@@ -262,7 +276,7 @@ def getSnowHeightUp(snow, table, se, query=None):
     :param query: Query Object which contains SQL query, if empty one will be created
     :returns: Query Object, can be reused for other queries
     """
-    if(table == "dwd"):
+    if(str(table) == "dwd"):
         if query is None:
             query = se.query(table).filter(table.c.snow_height >= snow)
             return query
@@ -272,12 +286,6 @@ def getSnowHeightUp(snow, table, se, query=None):
     else:
         print("No snowheigt available!")
         return query
-
-    #   if query is None:
-    #       query = se.query(table).filter(table.c.precipitation_type == "snow").filter(table.c.precipitation_amount >= snow)
-    #       return query
-    #   else:
-    #       query = query.filter(table.c.precipitation_type == "snow").filter(table.c.precipitation_amount >= snow)
 
 
 def getMaxTemp(table, se, query=None):
@@ -294,7 +302,7 @@ def getMaxTemp(table, se, query=None):
     else:
         return query.query(func.max(table.c.max_temp))
 
-# look for max temp
+
 def getMaxTempUp(maxTemp, table, se, query=None):
     r"""
     get all data from table with max_temp >= given value
@@ -310,6 +318,7 @@ def getMaxTempUp(maxTemp, table, se, query=None):
     else:
         query = query.filter(table.c.max_temp >= maxTemp)
         return query
+
 
 def getMaxTempDown(maxTemp, table, se, query=None):
     r"""
@@ -342,7 +351,7 @@ def getMinTemp(table, se, query=None):
     else:
         return query.query(func.min(table.c.min_temp))
 
-# look for min # temp
+
 def getMinTempUp(minTemp, table, se, query=None):
     r"""
     Returns table Data for the given Temperature upwards
@@ -358,6 +367,7 @@ def getMinTempUp(minTemp, table, se, query=None):
     else:
         query = query.filter(table.c.min_temp >= minTemp)
         return query
+
 
 def getMinTempDown(minTemp, table, se, query=None):
     r"""
@@ -376,7 +386,7 @@ def getMinTempDown(minTemp, table, se, query=None):
         query = query.filter(table.c.min_temp <= minTemp)
         return query
 
-# look for coverage
+
 def getCoverageUp(coverage, table, se, query=None):
     r"""
     Returns table Data for the given Coverage upwards
@@ -394,6 +404,7 @@ def getCoverageUp(coverage, table, se, query=None):
         query = query.filter(table.c.coverage_amount >= coverage)
         return query
 
+
 def getCoverageDown(coverage, table, se, query=None):
     r"""
     Returns table Data for the given Coverage downwards
@@ -409,7 +420,6 @@ def getCoverageDown(coverage, table, se, query=None):
         return query
     else:
         query = query.filter(table.c.coverage_amount <= coverage)
-
         return query
 
 
@@ -452,22 +462,30 @@ def getResult(query, se):
         se.close()
         return np.vstack(result)
 
+
+def read_username_password(path_to_login_file):
+    file = open(path_to_login_file, 'r')
+    username = (file.readline()[10:-1])
+    password = file.readline()[10:]
+    file.close()
+    return username,password
+
+
 #Setup Data
 def getConnectionData(tablename):
     try:
+        username, password = read_username_password(path_to_login_file)
         Base = declarative_base()
         engine = create_engine(
-            'mysql+pymysql://dwdtestuser:asdassaj14123@weather.service.tu-berlin.de/dwdtest?use_unicode=1&charset=utf8&ssl_cipher=AES128-SHA')
+            'mysql+pymysql://'+username+':'+password+'@weather.service.tu-berlin.de/dwdtest?use_unicode=1&charset=utf8&ssl_cipher=AES128-SHA')
         # mysql --ssl-cipher=AES128-SHA -u dwdtestuser -p -h weather.service.tu-berlin.de dwdtest
         # mysql --ssl -u dwdtestuser -p -h weather.service.tu-berlin.de dwdtest
-        # password asdassaj14123
-        # use dwdtest
         Base.metadata.create_all(engine)
         Session = sqla.orm.sessionmaker()
         Session.configure(bind=engine)
+        metadata = MetaData(engine, reflect=True)
         se = Session()
         # connect to database
-        metadata = MetaData(engine, reflect=True)
     except Exception as error:
         if "Engine not found" in str(error):
             print("Engine not found")
